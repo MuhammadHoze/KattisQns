5a71fd621b466cddfa31ac914728f827c4c24d3b
MuhammadHoze <42372471+MuhammadHoze@users.noreply.github.com>
2021-05-28 11:28:04 +0800

    Add files via upload

---
+++ Aaah!.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    string joninput;
+	string docinput;  
+    
+    
+    
+    cin >> joninput;
+    cin >> docinput;
+    
+    if (joninput.length() >= docinput.length() ) {
+    	
+    	cout << "go";
+    	
+	}
+	
+	else {
+		cout << "no";
+	}
+    
+       
+    
+    return 0;
+        
+}
